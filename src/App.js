ccde12bd2f4891f0ed265d3ca923492a5aadc259
kaveh-bazugir <bazugir@yahoo.com>
2022-06-05 03:03:04 +0430

    kir e kahar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {CalDisplay} from './calculatorFunctions'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>calculator</h1>
       </header>
+      <div>
+        <p>{myresult}</p>
+      </div>
+      <div>
+        <button onClick={Increament}>+</button>
+        <button>y</button>
+      </div>
     </div>
   );
+}
+let myresult = 0;
+function Increament() {
+  myresult+=1;
+  return {MyDisplay}
+}
+function MyDisplay() {
+  return <p>{myresult}</p>;
 }
 
 export default App;
---
+++ src/calculatorFunctions.js
@@ -0,0 +1,4 @@
+let calresult = 0;
+export function Increament() { return calresult++}
+export const CalDisplay = () => <p>{calresult}</p>;
+
