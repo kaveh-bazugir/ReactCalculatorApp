523a4519222aad15f470ff3dc43ba29cfd868e1d
kaveh-bazugir <bazugir@yahoo.com>
2022-06-17 19:35:09 +0430

    adding some new buttons

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -105,6 +105,20 @@ const Calculator = () => {
                 OperationFunction();
                 setLastOperator("");
                 break;
+            case "C":
+                setMyInput("0");
+                break;
+            case "CE":
+                setMyInput("0");
+                setMyResult(0);
+                break;
+            case "del":
+                if(myInput.length > 1) {
+                    setMyInput(myInput.slice(0,myInput.length - 1));
+                } else {
+                    setMyInput("0");
+                }
+                break;
             default:
                 setMyInput(parseFloat(myInput + btnInput).toString());
         }
@@ -115,25 +129,29 @@ const Calculator = () => {
             {label:"1", onClick:() => CalInput("1")},
             {label:"2", onClick:() => CalInput("2")},
             {label:"3", onClick:() => CalInput("3")},
-            {label:"+", onClick:() => CalInput("+")}
+            {label:"+", onClick:() => CalInput("+")},
+            {label:"^", onClick:() => CalInput("^")}
         ],
         [
             {label:"4", onClick:() => CalInput("4")},
             {label:"5", onClick:() => CalInput("5")},
             {label:"6", onClick:() => CalInput("6")},
-            {label:"-", onClick:() => CalInput("-")}
+            {label:"-", onClick:() => CalInput("-")},
+            {label:"C", onClick:() => CalInput("C")}
         ],
         [
             {label:"7", onClick:() => CalInput("7")},
             {label:"8", onClick:() => CalInput("8")},
             {label:"9", onClick:() => CalInput("9")},
-            {label:"*", onClick:() => CalInput("*")}
+            {label:"*", onClick:() => CalInput("*")},
+            {label:"CE", onClick:() => CalInput("CE")}
         ],
         [
             {label:"0", onClick:() => CalInput("0")},
             {label:".", onClick:() => CalInput(".")},
             {label:"=", onClick:() => CalInput("=")},
-            {label:"/", onClick:() => CalInput("/")}
+            {label:"/", onClick:() => CalInput("/")},
+            {label:"del", onClick:() => CalInput("del")}
         ]
     ];
 
